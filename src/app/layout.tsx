9696fde83b09b3b9530180c61ebf92d4b3dcb3d5
ChoaibMouhrach <cmouhrach@gmail.com>
2026-06-08 23:59:06 +0100

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
@@ -124,6 +125,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
